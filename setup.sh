ed1ebb82a623ff82c40b21ecf316aad1bcf1ec90
daohe <daohengliu@gmail.com>
2019-10-08 12:23:51 +0100

    Revert "Moved apt operations out of Docker file into setup"
    This reverts commit 4fab710b

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update \
-    && apt-get -y install --no-install-recommends python3 2>&1 \
-    && apt-get -y install --no-install-recommends apt-utils dialog 2>&1 \
-    && apt-get -y install git procps lsb-release \
-    && apt-get autoremove -y \
-    && apt-get clean -y \
-    && rm -rf /var/lib/apt/lists/*
