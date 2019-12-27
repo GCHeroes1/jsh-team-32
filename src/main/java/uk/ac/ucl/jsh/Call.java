fe42c898437b12a5441f95f502fc99d62bf8004e
Rajesh Goyal <zcabrgo@ucl.ac.uk>
2019-12-27 02:28:12 +0000

    After the chmod stuff, it wants me to add all the stuff again? i made a few changes, passed another test, revert if breaks pls...

--- src/main/java/uk/ac/ucl/jsh/Call.java
+++ src/main/java/uk/ac/ucl/jsh/Call.java
@@ -116,19 +116,27 @@ public class Call extends Jsh implements CommandInterface
 
     private String merge_collated_quotes(String command)
     {
-        String collated_quotes_regex = "([^\\s\"]*(\"[^\"]*\")*[^\\s\"]*)|([^\\s\']*(\'[^\']*\')*[^\\s\']*)";
+        String collated_quotes_regex = "([^\\s\"]*(\"[^\"]*\")*[^\\s\"]*)|([^\\s']*('[^']*')*[^\\s']*)";
         Pattern regex_pattern = Pattern.compile(collated_quotes_regex);
+        //command = command.replace("\'", "\"");
         Matcher regex_matcher = regex_pattern.matcher(command);
         ArrayList<String> pieces = new ArrayList<>();
         String match;
+
         while (regex_matcher.find())
         {
             match = regex_matcher.group();
-            if(match.indexOf('"') != -1)  // only put quotes around if the part contains quotes
+            if(match.indexOf('\'') != -1)  // only put quotes around if the part contains quotes
+            {
+                match = match.replace("\'", "");
+                match = "\'" + match + "\'";
+            }
+            if(match.indexOf('\"') != -1)  // only put quotes around if the part contains quotes
             {
                 match = match.replace("\"", "");
                 match = "\"" + match + "\"";
             }
+
             pieces.add(match);
         }
         return String.join(" ", pieces);
