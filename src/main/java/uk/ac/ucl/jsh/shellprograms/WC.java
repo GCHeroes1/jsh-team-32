2e5dc17b9c72ad339d0f7c0ca8aff881b972cde5
Rajesh Goyal <zcabrgo@ucl.ac.uk>
2019-12-28 15:39:09 +0000

    soz part 2

--- src/main/java/uk/ac/ucl/jsh/shellprograms/WC.java
+++ src/main/java/uk/ac/ucl/jsh/shellprograms/WC.java
@@ -38,16 +38,7 @@ public class WC extends ShellProgram {
         reader.close();
         return lines;
     }
-
-    private static byte[][] convertToBytes(String[] strings) {
-        byte[][] data = new byte[strings.length][];
-        for (int i = 0; i < strings.length; i++) {
-            String string = strings[i];
-            data[i] = string.getBytes(StandardCharsets.UTF_8); // you can chose charset
-        }
-        return data;
-    }
-
+    
     @Override
     public void execute(String[] args, InputStream stdin, OutputStream stdout) throws IOException
     {
