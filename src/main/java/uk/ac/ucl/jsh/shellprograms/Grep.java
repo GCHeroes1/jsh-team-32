9df3525428c1169f60fb69d7fd510662cf441059
daohe <daohengliu@gmail.com>
2019-12-31 01:51:19 +0000

    restructured grep to remove redundant (and resource leaky) loop

--- src/main/java/uk/ac/ucl/jsh/shellprograms/Grep.java
+++ src/main/java/uk/ac/ucl/jsh/shellprograms/Grep.java
@@ -37,7 +37,6 @@ public class Grep extends ShellProgram
             int numOfFiles = args.length - 1;
             Path filePath;
             BufferedReader reader;
-            Path[] filePathArray = new Path[numOfFiles];
             Path currentDir = Paths.get(currentDirectory);
             Charset encoding = StandardCharsets.UTF_8;
             for (int i = 0; i < numOfFiles; i++)
@@ -45,29 +44,25 @@ public class Grep extends ShellProgram
                 filePath = currentDir.resolve(args[i + 1]);
                 try
                 {
-                    Files.newBufferedReader(filePath, encoding);
+                    reader = Files.newBufferedReader(filePath, encoding);
+                    String line;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        Matcher matcher = grepPattern.matcher(line);
+                        if (matcher.find())
+                        {
+                            str_to_bytes.write(line);
+                            str_to_bytes.write(System.getProperty("line.separator"));
+                            str_to_bytes.flush();
+                        }
+                    }
                 }
                 catch (IOException e)
                 {
                     throw new RuntimeException("grep: wrong file argument");
                 }
-                filePathArray[i] = filePath;
             }
-            for (Path path : filePathArray)
-            {
-                reader = Files.newBufferedReader(path, encoding);
-                String line;
-                while ((line = reader.readLine()) != null)
-                {
-                    Matcher matcher = grepPattern.matcher(line);
-                    if (matcher.find())
-                    {
-                        str_to_bytes.write(line);
-                        str_to_bytes.write(System.getProperty("line.separator"));
-                        str_to_bytes.flush();
-                    }
-                }
-            }
+
         }
 
     }
